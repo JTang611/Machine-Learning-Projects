576b44e3ce3905f90b769306e45d41ae12a7b1ea
Jiahong Tang <65364425+JTang611@users.noreply.github.com>
2020-09-29 11:21:41 +0200

    Add files via upload

---
+++ CIFAR_Classifier_SVM.py
@@ -0,0 +1,232 @@
+# Change directory to VSCode workspace root so that relative path loads work correctly. Turn this addition off with the DataScience.changeDirOnImportExport setting
+# ms-python.python added
+import os
+try:
+	os.chdir(os.path.join(os.getcwd(), '..'))
+	print(os.getcwd())
+except:
+	pass
+
+
+# Run some setup code for this notebook.
+import random
+import numpy as np
+from ./data_utils import load_CIFAR10
+import matplotlib.pyplot as plt
+
+# This is a bit of magic to make matplotlib figures appear inline in the
+# notebook rather than in a new window.
+get_ipython().run_line_magic('matplotlib', 'inline')
+plt.rcParams['figure.figsize'] = (10.0, 8.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+# Some more magic so that the notebook will reload external python modules;
+# see http://stackoverflow.com/questions/1907993/autoreload-of-modules-in-ipython
+get_ipython().run_line_magic('load_ext', 'autoreload')
+get_ipython().run_line_magic('autoreload', '2')
+
+
+# Load the raw CIFAR-10 data.
+cifar10_dir = 'datasets/cifar-10-batches-py'
+X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
+
+# As a sanity check, we print out the size of the training and test data.
+print('Training data shape: ', X_train.shape)
+print('Training labels shape: ', y_train.shape)
+print('Test data shape: ', X_test.shape)
+print('Test labels shape: ', y_test.shape)
+
+
+# Split the data into train, val, and test sets. In addition we will
+# create a small development set as a subset of the training data;
+# we can use this for development so our code runs faster.
+num_training = 49000
+num_validation = 1000
+num_test = 1000
+num_dev = 500
+
+# Our validation set will be num_validation points from the original
+# training set.
+mask = range(num_training, num_training + num_validation)
+X_val = X_train[mask]
+y_val = y_train[mask]
+
+# Our training set will be the first num_train points from the original
+# training set.
+mask = range(num_training)
+X_train = X_train[mask]
+y_train = y_train[mask]
+
+# We will also make a development set, which is a small subset of
+# the training set.
+mask = np.random.choice(num_training, num_dev, replace=False)
+X_dev = X_train[mask]
+y_dev = y_train[mask]
+
+# We use the first num_test points of the original test set as our
+# test set.
+mask = range(num_test)
+X_test = X_test[mask]
+y_test = y_test[mask]
+
+print('Train data shape: ', X_train.shape)
+print('Train labels shape: ', y_train.shape)
+print('Validation data shape: ', X_val.shape)
+print('Validation labels shape: ', y_val.shape)
+print('Test data shape: ', X_test.shape)
+print('Test labels shape: ', y_test.shape)
+
+
+
+# Preprocessing: reshape the image data into rows
+X_train = np.reshape(X_train, (X_train.shape[0], -1))
+X_val = np.reshape(X_val, (X_val.shape[0], -1))
+X_test = np.reshape(X_test, (X_test.shape[0], -1))
+X_dev = np.reshape(X_dev, (X_dev.shape[0], -1))
+
+# As a sanity check, print out the shapes of the data
+print('Training data shape: ', X_train.shape)
+print('Validation data shape: ', X_val.shape)
+print('Test data shape: ', X_test.shape)
+print('dev data shape: ', X_dev.shape)
+
+# Preprocessing: subtract the mean image
+# first: compute the image mean based on the training data
+mean_image = np.mean(X_train, axis=0)
+print(mean_image[:10]) # print a few of the elements
+plt.figure(figsize=(4,4))
+plt.imshow(mean_image.reshape((32,32,3)).astype('uint8')) # visualize the mean image
+plt.show()
+
+# second: subtract the mean image from train and test data
+X_train -= mean_image
+X_val -= mean_image
+X_test -= mean_image
+X_dev -= mean_image
+
+# third: append the bias dimension of ones (i.e. bias trick) so that our SVM
+# only has to worry about optimizing a single weight matrix W.
+X_train = np.hstack([X_train, np.ones((X_train.shape[0], 1))])
+X_val = np.hstack([X_val, np.ones((X_val.shape[0], 1))])
+X_test = np.hstack([X_test, np.ones((X_test.shape[0], 1))])
+X_dev = np.hstack([X_dev, np.ones((X_dev.shape[0], 1))])
+
+print(X_train.shape, X_val.shape, X_test.shape, X_dev.shape)
+
+
+import time
+from cs231n.classifiers.linear_svm import svm_loss_vectorized
+
+# Initialize the gradient to be a random matrix with small numbers
+W = np.random.randn(3073, 10) * 0.0001
+
+# Get the vectorized loss
+tic = time.time()
+loss_vectorized, _ = svm_loss_vectorized(W, X_dev, y_dev, 0.000005)
+toc = time.time()
+print('Vectorized loss: %e computed in %fs' % (loss_vectorized, toc - tic))
+
+
+
+from linear_classifier import LinearSVM
+
+# Initialize the classifier object
+svm = LinearSVM()
+tic = time.time()
+loss_hist = svm.train(X_train, y_train, learning_rate=1e-7, reg=2.5e4,
+                      num_iters=1500, verbose=True)
+toc = time.time()
+print('That took %fs' % (toc - tic))
+
+# A useful debugging strategy is to plot the loss as a function of
+# iteration number:
+plt.plot(loss_hist)
+plt.xlabel('Iteration number')
+plt.ylabel('Loss value')
+plt.show()
+
+# Write the LinearSVM.predict function and evaluate the performance on both the
+# training and validation set
+y_train_pred = svm.predict(X_train)
+print('training accuracy: %f' % (np.mean(y_train == y_train_pred), ))
+y_val_pred = svm.predict(X_val)
+print('validation accuracy: %f' % (np.mean(y_val == y_val_pred), ))
+
+# Use the validation set to tune hyperparameters (regularization strength and
+# learning rate). You should experiment with different ranges for the learning
+# rates and regularization strengths; if you are careful you should be able to
+# get a classification accuracy of about 0.4 on the validation set.
+learning_rates = [3.45e-7, 3.7e-7, 4e-7, 5e-7, 3e-7]
+regularization_strengths = [6e4, 6.4e4, 6.5e4, 7e4, 5e4]
+
+# results is dictionary mapping tuples of the form
+# (learning_rate, regularization_strength) to tuples of the form
+# (training_accuracy, validation_accuracy). The accuracy is simply the fraction
+# of data points that are correctly classified.
+results = {}
+best_val = -1   # The highest validation accuracy that we have seen so far.
+best_svm = None # The LinearSVM object that achieved the highest validation rate.
+
+def compute_accuracy(y, y_pred):
+    return np.mean(y == y_pred)
+
+for lr in learning_rates:
+    for reg in regularization_strengths:
+        svm = LinearSVM()
+        svm.train(X_train, y_train, lr, reg, 1500)
+
+        y_pred = svm.predict(X_val)
+        val_accuracy = compute_accuracy(y_val, y_pred)
+        print('lr: %.8f, reg: %5.1f - validation accuracy: %2.2f' %(lr, reg, val_accuracy))
+
+        results[(lr, reg)] = val_accuracy
+
+        if val_accuracy > best_val:
+            best_val = val_accuracy
+            best_svm = svm
+
+
+for lr, reg in sorted(results):
+        val_accuracy = results[(lr, reg)]
+        print('lr %e reg %e val accuracy: %f' % (
+                    lr, reg, val_accuracy))
+
+print('best validation accuracy achieved during cross-validation: %f' % best_val)
+
+# Visualize the cross-validation results
+import math
+x_scatter = [math.log10(x[0]) for x in results]
+y_scatter = [math.log10(x[1]) for x in results]
+
+# plot validation accuracy
+colors = [results[x] for x in results] # default size of markers is 20
+plt.subplot(2, 1, 2)
+plt.scatter(x_scatter, y_scatter, marker_size, c=colors)
+plt.colorbar()
+plt.xlabel('log learning rate')
+plt.ylabel('log regularization strength')
+plt.title('CIFAR-10 validation accuracy')
+plt.show()
+
+
+# Evaluate the best svm on test set
+y_test_pred = best_svm.predict(X_test)
+test_accuracy = np.mean(y_test == y_test_pred)
+print('linear SVM on raw pixels final test set accuracy: %f' % test_accuracy)
+
+# Visualize the learned weights for each class.
+# Depending on your choice of learning rate and regularization strength, these may
+# or may not be nice to look at.
+w = best_svm.W[:-1,:] # strip out the bias
+w = w.reshape(32, 32, 3, 10)
+w_min, w_max = np.min(w), np.max(w)
+classes = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+for i in range(10):
+    plt.subplot(2, 5, i + 1)
+
+    # Rescale the weights to be between 0 and 255
+    wimg = 255.0 * (w[:, :, :, i].squeeze() - w_min) / (w_max - w_min)
+    plt.imshow(wimg.astype('uint8'))
+    plt.axis('off')
+    plt.title(classes[i])
